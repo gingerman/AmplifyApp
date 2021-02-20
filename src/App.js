39baf17e21d20e96103585080d9c4e6c358af58b
gingerman <steve@gingerman.co.uk>
2021-02-20 13:35:10 +0000

    test 16

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.gingerman.co.uk"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Steve Says CHIMMICHANGA 
+          Go and see gingerman.co.uk 
         </a>
       </header>
     </div>
