08fba5aca08220ce99beaf04c015bf438fb2a2e6
gingerman <steve@gingerman.co.uk>
2021-02-20 13:38:59 +0000

    test 17

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="Sodium-Design-App-logo" alt="Sodium Design Logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A design company not a mass production company.
         </p>
         <a
           className="App-link"
